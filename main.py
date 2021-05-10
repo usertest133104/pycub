1d0411b98198d451a3783b89f8ebaa254a5881af
gonpvp <gonpvpmc@gmail.com>
2021-05-10 20:31:23 +0200

    PyCub Alpha 03
    Added PyCub_Write() and PyCub_Import function to write code much easier, this will be useful to create your own addon very easily!

--- main.py
+++ main.py
@@ -2,17 +2,34 @@
 # IMPORTS
 
 import string
+import time
+import os
 
 # BASE NON COMPILED FUNCTION
 
 def PyCub_Compil():
+
+
+    
+    def PyCub_Write(var):
+        PyCub_C_Write.append(var)
+    def PyCub_Import(var):
+        PyCub_I_Write.append(var)
+    
+    
+    def PyCub_AddImport(imports):
+        with open(TargetFile, "r+", encoding='utf8') as f:
+            old = f.read() # read everything in the file
+            f.seek(0) # rewind
+            f.write("new linesetset\n" + old) # write the new line before
+
 
     # LIST OF ALL EXPRESSION 
 
     def PyCub_AddExpr(car, event):
         if car.contains("player"):
             if event == "on_player_join":
-                Target.write(tab * 2 + "Bukkit.broascastMessage(" + reason + ") ;\n")
+                PyCub_Write(tab * 2 + "Bukkit.broascastMessage(" + reason + ") ;\n")
 
     # LIST OF ALL EFFECT
 
@@ -26,7 +43,7 @@ def PyCub_Compil():
                 reason = reason.replace("+ player +", '+ event.getPlayer().getName() +')
                 reason = reason.replace("player +", 'event.getPlayer().getName() +')
                 reason = reason.replace("+ player", '+ event.getPlayer().getName()')
-                Target.write(tab * 2 + "Bukkit.broascastMessage(" + reason + ");\n")  
+                PyCub_Write(tab * 2 + "Bukkit.broascastMessage(" + reason + ");\n")  
         elif car.startswith('broadcast'):
             expr = car
             expr = expr[10:]
@@ -35,38 +52,46 @@ def PyCub_Compil():
             reason = reason.replace("+ player +", '+ event.getPlayer().getName() +')
             reason = reason.replace("player +", 'event.getPlayer().getName() +')
             reason = reason.replace("+ player", '+ event.getPlayer().getName()')
-            Target.write(tab * 2 + "Bukkit.broascastMessage(" + reason + ");\n")   
+            PyCub_Write(tab * 2 + "Bukkit.broascastMessage(" + reason + ");\n")   
 
     print("Loading ...")
 
-    From = 'C:/PythonProject/Pycub/test.pycub'
-    Target = 'C:/PythonProject/Pycub/test.txt'
-
-    From = open(From, 'r', encoding='utf8')
-    Target = open(Target, 'w', encoding='utf8')
+    FromFile = 'C:/PythonProject/Pycub/scripts/test.pycub'
+    
+    ProjectName = 'test/'
+    PathTargetFile = 'C:/PythonProject/Pycub/projects/' + ProjectName
+    
+    TargetFile = PathTargetFile + '/test.txt'
+    TargetCacheFile = 'C:/PythonProject/Pycub/scripts/.cache/test.pycub'
+    
+    os.makedirs(os.path.dirname(PathTargetFile), exist_ok=True)
+    From = open(FromFile, 'r', encoding='utf8')
+    Target = open(TargetFile, 'w+', encoding='utf8')
 
     tab = "    "
     first_event = False
     global lign
     lign = 1
     eventplayerjoin = 0
-
+    PyCub_C_Write = []
+    PyCub_C_Import = []
+    
     for car in From:
         
         # Sélectionneur d'évènement
         lign =+ 1
         
         if car.startswith('package'):
-            Target.write(car + ';')
+            PyCub_Write(car + ';')
         if car.startswith('#'):
-            Target.write(car)
-        elif car.startswith('import'):
-            if car.startswith('import bukkit'):
-                Target.write('import org.bukkit;\n')
-            
-            else: 
-                text = car + ';'
-                Target.write(text)
+            PyCub_Write(car)
+#        elif car.startswith('import'):
+#            if car.startswith('import bukkit'):
+#                PyCub_Write('import org.bukkit;\n')
+#            
+#            else: 
+#                text = car + ';'
+#                PyCub_Write(text)
         
         elif car.startswith('event') or car.startswith('on'):
             
@@ -81,7 +106,7 @@ def PyCub_Compil():
             # ADD FIRST PUBLIC CLASS
             
             if first_event == False:
-                Target.write('public class PycubEvent implements Listener {\n')
+                PyCub_Write('public class PycubEvent implements Listener {\n')
                 first_event = True
             
             # LIST OF ALL EVENT 
@@ -89,13 +114,15 @@ def PyCub_Compil():
 
             if event.startswith('on_player_join:') or event.startswith('player join:'):
                 
+
+                PyCub_AddImport("igg\n esse")  
                 # écriture de l'handler évent
                 
-                Target.write('\n' + tab + '@EventHandler \n')
+                PyCub_Write('\n' + tab + '@EventHandler \n')
                 
                 # NAME OF PUBLIC CLASS
                 
-                Target.write(tab + 'public void OnPlayerJoin' + str(eventplayerjoin) + '(PlayerJoinEvent event) {\n')
+                PyCub_Write(tab + 'public void OnPlayerJoin' + str(eventplayerjoin) + '(PlayerJoinEvent event) {\n')
                 expr_lign = lign
                 for _expr in From:
                     expr = _expr.strip()
@@ -106,10 +133,10 @@ def PyCub_Compil():
                     else:
                         PyCub_AddEffect(expr, "on_player_join")
                 
-                Target.write(tab * 1 + '}\n')                    
+                PyCub_Write(tab * 1 + '}\n')                  
                         
         else:        
-            Target.write('\n')
+            PyCub_Write('\n')
             
             acar = car
             acar.replace(" ", "")
@@ -119,8 +146,14 @@ def PyCub_Compil():
                     print("The event is not exist lign" + str(lign))
 
     if first_event == True:
-        Target.write('}\n')
+        PyCub_Write('}\n')
 
+    for var in PyCub_C_Import:
+        Target.write(var)
+    for var in PyCub_C_Write:
+        Target.write(var)
+
+        
     # fermeture du fichier avec la méthode close()
     Target.close()
     From.close()
--- run.bat
+++ run.bat
@@ -1,4 +1,3 @@
-cd C:\PythonProject\Pycub
 py -3.7 -m venv env
 env\Scripts\activate.bat
-python main.py runserver
+python main.py
